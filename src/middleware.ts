a521617206bd085e7f4d4bdd21a8c311fa101ca0
Parth Malhotra <malhotraparth2004@gmail.com>
2025-06-28 21:20:30 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,8 @@
-import { auth } from "@/auth";
-
-export default auth;
+export { auth as middleware } from "@/auth";
 
 // The matcher configuration prevents the middleware from running on specified paths.
 // It protects all routes except for the auth API routes, Next.js specific files, public assets, and the login/signup pages.
 export const config = {
-  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|login|signup).*)"],
+  matcher: ["/((?!api/auth|_next/static|_next/image|favicon\\.ico|login|signup).*)"],
   runtime: 'nodejs',
 }
