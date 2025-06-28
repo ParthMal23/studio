d498a50edea5c1e7ecad8a949edf262f8a75c48c
Parth Malhotra <malhotraparth2004@gmail.com>
2025-06-28 23:07:16 +0000

    this button is still using blue when i hover

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent-hover hover:text-accent-hover-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-accent-hover hover:text-accent-hover-foreground",
         ghost: "hover:bg-accent-hover hover:text-accent-hover-foreground",
